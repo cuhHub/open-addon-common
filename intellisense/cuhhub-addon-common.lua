880b07429ac2c43b9793c0b02ea32c0c86a6d99c
Cuh4 <89425262+Cuh4@users.noreply.github.com>
2026-07-06 20:35:42 +0100

    feat(intellisense): add `Addon.Logger` definition

---
+++ intellisense/cuhhub-addon-common.lua
@@ -0,0 +1,6 @@
+--[[
+    A file to add intellisense for common variables and functions in all cuhHub addons.<br>
+    Do not import this in your projects.
+]]
+
+Addon.Logger = Noir.Libraries.Logging:CreateLogger("...")
